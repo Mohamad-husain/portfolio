f44ba26402fa1a9355af0dd22b806bba96dcdfa0
Mohamad-husain <144700473+Mohamad-husain@users.noreply.github.com>
2025-11-21 23:51:28 +0200

    last update

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {}
+export class ContactComponent {
+  @ViewChild('contactForm') contactForm!: ElementRef<HTMLFormElement>;  // الحصول على الفورم HTML
+
+  sendEmail() {
+    if (this.contactForm) {
+      emailjs.sendForm(
+        'service_ma6i7kc',
+        'template_3zj0ubu',
+        this.contactForm.nativeElement,   // ✅ هنا نمرر العنصر HTML مباشرة
+        'K4_GaG8asrg_lmMZS'
+      ).then(
+        (result) => {
+          alert('Message sent successfully!');
+          this.contactForm.nativeElement.reset();
+        },
+        (error) => {
+          alert('Failed to send message. Please try again.');
+          console.log(error.text);
+        }
+      );
+    }
+  }
+}
